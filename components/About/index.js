a43f9f0f35d43790534a5a2991fd48ca7f12e4ad
“electricganesha” <christian.marques@gmail.com>
2021-11-10 17:45:16 +0000

    Remove about header

--- components/About/index.js
+++ components/About/index.js
@@ -4,7 +4,6 @@ import styles from './About.module.scss'
 const About = props => {
     return (
         <div className={styles.about}>
-            <h1>What is HYPSEUS?</h1>
             <p>The idea for project HYPSEUS came about during the 2020 pandemic when I started asking people for recommendations and lists of stuff to watch / listen / read / play
                 during this time where it was hard to find a center and fully appreciate things with all the strangeness of the period. Thinking about this exchange and a very
                 interesting <a href="https://www.youtube.com/watch?v=Wzj4h0R_ryQ">video</a> I had seen regarding the relationship of video-games with the <a href="https://en.wikipedia.org/wiki/Sublime_(philosophy)">
